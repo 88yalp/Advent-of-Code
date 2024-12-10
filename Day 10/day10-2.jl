2a4543d10ad4b51b095936c13261eecc4378585a
Magnus Rein <Magnus.Reinstudent.uib.no>
2024-12-10 13:28:06 +0100

    Solved day10 part 2

---
+++ Day 10/day10-2.jl
@@ -0,0 +1,78 @@
+import Base.+,Base.-
+
+function main()
+    cd("C:\\Users\\sunga\\OneDrive - University of Bergen\\fritid\\koding\\Advent-of-Code\\Day 10")
+    
+    #file = "example_dataset.txt"
+    file = "dataset.txt"
+    
+    layout = read(file, String)
+
+    global cols = findfirst('\n', layout) -2
+    global rows = length(split(layout, "\n")) - 1 
+
+    answer = 0
+    
+    for source_frequency in eachmatch(r"0", layout)
+        answer += find_unique_tops(source_frequency.offset, layout)
+    end
+
+    return answer
+    
+end
+
+function find_unique_tops(trailhead::Int, layout::String)
+    rating = 0
+
+    rating += find_tops(index_to_cords(trailhead), layout)
+
+    return rating
+end 
+
+function find_tops(cords::Tuple{Int, Int}, layout::String)
+    if parse(Int, layout[cords_to_index(cords)]) == 9
+        return 1
+    end
+    directions = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+    rating = 0
+
+    for direction in directions
+        new_cords = cords + direction
+        if is_in_layout(new_cords)
+            if parse(Int, layout[cords_to_index(cords)]) + 1 == parse(Int, layout[cords_to_index(new_cords)]) 
+                rating += find_tops(new_cords, layout)
+            end
+        end
+    end
+    return rating
+end
+    
+
+function index_to_cords(index::Int) 
+    return  fldmod1(index, cols+2)
+end
+
+function cords_to_index((row, col)::Tuple{Int, Int})
+    return (row-1) * (cols+2) + col 
+end
+
+function is_in_layout(cords::Tuple{Int, Int})
+    if cords[1] < 1 || cords[1] > rows
+        return false
+    end
+    if cords[2] < 1 || cords[2] > cols 
+        return false
+    end
+    return true
+end
+
+function +(a::Tuple{Int, Int}, b::Tuple{Int, Int})
+    return (a[1]+b[1],a[2]+b[2])
+end
+
+function -(a::Tuple{Int, Int}, b::Tuple{Int, Int})
+    return (a[1]-b[1],a[2]-b[2])
+end
+    
+println(main())
+
